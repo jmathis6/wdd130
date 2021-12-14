f37aec664d78a9cd6e69dfd8b2e392659aaaca5d
jmathis6 <juliejmathis1@gmail.com>
2021-12-13 23:04:38 -0700

    add javascript

--- finalproject/script.js
+++ finalproject/script.js
@@ -6,7 +6,15 @@ contents.forEach(content => {
         content.nextElementSibling.getElementsByClassName.display = "none";
     }
     else{
-        let displayText = content.textConetnt.slice (0, noOfCharac);
-        let moreText = content.textContent
+        let displayText = content.textContent.slice (0, noOfCharac);
+        let moreText = content.textContent.slice (noOfCharac);
+        content.innerHTML = `${displayText}<span class ="dots">...</span><span class="hide more">${moreText}</span>`;
     }
-})
+});
+
+function readMore(btn){
+        let article = btn.parentElement;
+        article.querySelector(".dots").classList.toggle("hide");
+        postMessage.querySelector(".more").classList.toggle("hide");
+        btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
+}
